8c58c7b37ee369dd4df695f4a34515dca8e6ca94
sosa <sosanagi@gmail.com>
2021-11-02 15:07:35 +0900

    H2DBsql実行までp49

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 p42. 
 @see https://marketplace.visualstudio.com/items?itemName=GabrielBB.vscode-lombok
+
+p47.
+追加の方法がよくわからないので、環境構築からやり直しました。
+以下3つをDependenciesに追記
+- Lombok
+- JDBC API
+- H2 Database
--- main/java/com/example/HajibootDIApplication.java
+++ main/java/com/example/HajibootDIApplication.java
@@ -1,24 +1,25 @@
 package com.example;
 
-import com.example.domain.Customer;
-import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 @SpringBootApplication
 public class HajibootDIApplication implements CommandLineRunner{
     @Autowired
-    CustomerService customerService;
+    NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public void run(String... strings) throws Exception {
-        customerService.save(new Customer(1, "Nobita", "Nobi"));
-        customerService.save(new Customer(2, "Takeshi", "Goda"));
-        customerService.save(new Customer(3, "Suneo", "Honekawa"));
+        String sql = "SELECT 1";
+        SqlParameterSource param = new MapSqlParameterSource();
+        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
 
-        customerService.findAll().forEach(System.out::println);
+        System.out.println("result = "+ result);
     }
 
     public static void main(String[] args) {
