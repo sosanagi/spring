4d0052760883edd5c5527cea78dd581c7fdcfae8
sosa <sosanagi@gmail.com>
2021-11-02 13:07:03 +0900

    Frontend不要(p40)

--- main/java/com/example/HajibootDIApplication.java
+++ main/java/com/example/HajibootDIApplication.java
@@ -1,15 +1,29 @@
 package com.example;
 
-import com.example.app.Frontend;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class HajibootDIApplication {
+public class HajibootDIApplication implements CommandLineRunner{
+    @Autowired
+    ArgumentResolver argumentResolver;
+    @Autowired
+    Calculator calculator;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.print("Enter 2 numbers like 'a b' :");
+        Argument argument = argumentResolver.resolve(System.in);
+        int result = calculator.calc(argument.getA(), argument.getB());
+        System.out.println("result = " + result);
+    }
+
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(HajibootDIApplication.class, args);
-        Frontend frontend = context.getBean(Frontend.class);
-        frontend.run();
+        SpringApplication.run(HajibootDIApplication.class, args);
     }
 }
