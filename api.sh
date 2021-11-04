e18f2a1045564494caee8763118e197b3dbc243c
sosa <sosanagi@gmail.com>
2021-11-04 15:12:01 +0900

    add:api.sh_ex3.2.2_p85

---
+++ api.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl http://localhost:8080/api/customers/ -w "\n"
+echo "GET"
+curl http://localhost:8080/api/customers/1 -i -XGET -w "\n"
+echo "POST: Tamako"
+curl http://localhost:8080/api/customers/ -i -XPOST -H "Content-Type: application/JSON" -d '{"firstName":"Tamako","lastName":"Nobi"}' -w "\n"
+echo "Tamako check"
+curl http://localhost:8080/api/customers/ -w "\n"
+echo "PUT: Nobita->Nobio"
+curl http://localhost:8080/api/customers/1 -i -XPUT -H "Content-Type: application/JSON" -d '{"firstName":"Nobio","lastName":"Nobi"}' -w "\n"
+echo "Nobio check"
+curl http://localhost:8080/api/customers/1 -w "\n"
+echo "DELETE: Nobio"
+curl http://localhost:8080/api/customers/1 -i -XDELETE -w "\n"
+echo "Nobio check"
+curl http://localhost:8080/api/customers/1 -w "\n"
