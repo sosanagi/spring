02e2f4e6fddc1303e923c5d93f6a513564f6a6ad
sosa <sosanagi@gmail.com>
2021-11-04 11:50:01 +0900

    ページングまでp72

--- main/java/com/example/HajibootDIApplication.java
+++ main/java/com/example/HajibootDIApplication.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @SpringBootApplication
 public class HajibootDIApplication implements CommandLineRunner {
@@ -18,7 +21,14 @@ public class HajibootDIApplication implements CommandLineRunner {
         Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
         System.out.println(created + " is created!");
 
-        customerRepository.findAllOrderByName().forEach(System.out::println);
+        Pageable pageable = PageRequest.of(0,3);
+        Page<Customer> page = customerRepository.findAll(pageable);
+        System.out.println("1ページのデータ数=" + page.getSize());
+        System.out.println("現在のページ=" + page.getNumber());
+        System.out.println("全ページ数=" + page.getTotalPages());
+        System.out.println("全データ数=" + page.getTotalElements());
+
+        page.getContent().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
