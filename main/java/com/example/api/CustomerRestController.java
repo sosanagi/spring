43f3f623c19d917eae843a7570d50b90e02e6940
sosa <sosanagi@gmail.com>
2021-11-04 15:44:10 +0900

    ページングAPI_ex3.2.3_p89

--- main/java/com/example/api/CustomerRestController.java
+++ main/java/com/example/api/CustomerRestController.java
@@ -3,13 +3,15 @@ package com.example.api;
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("api/customers")
@@ -18,8 +20,8 @@ public class CustomerRestController {
     CustomerService customerService;
 
     @GetMapping
-    List<Customer> getCustomers() {
-        List<Customer> customers = customerService.findAll();
+    Page<Customer> getCustomers(@PageableDefault Pageable pageable) {
+        Page<Customer> customers = customerService.findAll(pageable);
         return customers;
     }
 
